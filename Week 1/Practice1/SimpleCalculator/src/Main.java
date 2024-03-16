ed853989e56644d8b6f4faa11255fd17bc56d36f
ypalHubxD <163189034+ypalHubxD@users.noreply.github.com>
2024-03-16 17:26:59 +0200

    Update Main.java

--- Week 1/Practice1/SimpleCalculator/src/Main.java
+++ Week 1/Practice1/SimpleCalculator/src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         double num1 = scanner.nextDouble();
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();
-        System.out.println("Select operation: ");
-        System.out.println("1. Addition (+)");
-        System.out.println("2. Subtraction (-)");
+        System.out.print("Select operation: \n");
+        System.out.print("1. Addition (+)\n");
+        System.out.print("2. Subtraction (-)\n");
         System.out.println("3. Multiplication (*)");
         System.out.println("4. Division (/)");
         System.out.print("Enter your choice: ");
