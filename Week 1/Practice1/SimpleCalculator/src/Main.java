f4f87b5de31fe534342a6be77e0363567bf1bbfe
ypalHubxD <163189034+ypalHubxD@users.noreply.github.com>
2024-03-16 17:25:36 +0200

    Update Main.java

--- Week 1/Practice1/SimpleCalculator/src/Main.java
+++ Week 1/Practice1/SimpleCalculator/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
+        System.out.println("Enter the first number: ");
         double num1 = scanner.nextDouble();
         System.out.print("Enter the second number: ");
         double num2 = scanner.nextDouble();
